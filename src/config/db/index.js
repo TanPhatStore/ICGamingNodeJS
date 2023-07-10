7ca9686ad85109e7e435299520acbf9a922dffe8
Vũ Tiến Đức <107258145+TanPhatStore@users.noreply.github.com>
2023-07-11 01:15:22 +0700

    t

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -17,7 +17,8 @@ async function connect () {
     setInterval(async () => {
         try {
             const d = new Date().toLocaleString("vi-VN", { timeZone: "Asia/Ho_Chi_Minh" }).split(' ')[0]
-            if (parseInt(d.split(':')[0]) == 0 && parseInt(d.split(':')[1]) == 0 && parseInt(d.split(':')[2]) == 0) {
+            console.log(d)
+            if (parseInt(d.split(':')[0]) == 1 && parseInt(d.split(':')[1]) == 16 && parseInt(d.split(':')[2]) == 0) {
                 await Game.updateMany({}, {second : 0, downloads : 0 })
             }
         } catch (error) {
